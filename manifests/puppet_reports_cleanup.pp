1369d70ac42cd388eb86e28fb3ba242a8c537899
PrinceSalatyell <salatyelll14@gmail.com>
2026-03-31 14:24:10 +0100

    P8: migrate puppet_infrastructure::puppet_reports_cleanup

--- manifests/puppet_reports_cleanup.pp
+++ manifests/puppet_reports_cleanup.pp
@@ -1,16 +1,22 @@
-### Purpose ########
-# This class cleans up puppet reports to avoid filling the disk
+# puppet_infrastructure::puppet_reports_cleanup
+#
+# Removes Puppet report files older than the configured retention period to
+# prevent unbounded growth in the report directory.
+#
+# Dependencies:
+# - Hiera data for `puppet::reportdir`
+# - Hiera data for `puppet::reportdays`
 class puppet_infrastructure::puppet_reports_cleanup {
 
   $reportdir  = lookup('puppet::reportdir')
   $reportdays = lookup('puppet::reportdays')
 
   cron { 'puppet_report_cleanup':
-  command  => "find ${reportdir} -type f -mtime +${reportdays} | xargs rm",
-  user     => root,
-  monthday => '*',
-  hour     => '0',
-  minute   => '0',
+    command  => "find ${reportdir} -type f -mtime +${reportdays} | xargs rm",
+    user     => root,
+    monthday => '*',
+    hour     => '0',
+    minute   => '0',
   }
 
 }
