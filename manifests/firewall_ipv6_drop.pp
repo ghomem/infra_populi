a8aa30c8fc0fbb6ff5c5773bacca8e7f78d92ade
PrinceSalatyell <salatyelll14@gmail.com>
2026-04-29 12:09:27 +0100

    P8: migrate puppet_infrastructure::firewall_ipv6_drop

--- manifests/firewall_ipv6_drop.pp
+++ manifests/firewall_ipv6_drop.pp
@@ -1,20 +1,22 @@
-### Purpose ########
-# This class provides an add on to firewall_secure that DROPs all IPv6 traffic
-
-### Dependencies ###
-#  modules: puppetlabs-firewall
+# @summary Drop inbound and forwarded IPv6 traffic.
+#
+# Applies IPv6 filter-table rules that drop all traffic entering the INPUT and
+# FORWARD chains. Intended as an add-on to the base firewall policy.
+#
+# @dependencies
+# - puppetlabs-firewall
 class puppet_infrastructure::firewall_ipv6_drop {
 
   firewall { '600 Drop ALL IPv6 - INPUT':
     chain    => 'INPUT',
     proto    => 'all',
-    action   => drop,
-    provider => 'ip6tables',
+    jump     => 'DROP',
+    protocol => 'IPv6',
   }
   firewall { '601 Drop ALL IPv6 - FORWARD':
     chain    => 'FORWARD',
     proto    => 'all',
-    action   => drop,
-    provider => 'ip6tables',
+    jump     => 'DROP',
+    protocol => 'IPv6',
   }
 }
