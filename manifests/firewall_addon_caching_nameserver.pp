c844283fc5ff037b8d994e7318c258d9daa99b38
PrinceSalatyell <salatyelll14@gmail.com>
2026-04-29 11:35:58 +0100

    P8: migrate puppet_infrastructure::firewall_addon_caching_nameserver

--- manifests/firewall_addon_caching_nameserver.pp
+++ manifests/firewall_addon_caching_nameserver.pp
@@ -1,13 +1,16 @@
-### Purpose ########
-# This class provides necessary firewall configurations for caching nameservers
+# @summary Allow LAN clients to query the local caching nameserver.
+#
+# @dependencies
+# - puppetlabs-firewall
 
 class puppet_infrastructure::firewall_addon_caching_nameserver (
-
-    $lan_iface,
-    $port = '53',
-
-){
-
-    firewall { '1301 accept nameserver': proto => 'udp', iniface => $lan_iface, dport => $port, action => 'accept' }
-
+  $lan_iface,
+  $port = '53',
+) {
+  firewall { '1301 accept nameserver':
+    proto   => 'udp',
+    iniface => $lan_iface,
+    dport   => $port,
+    jump    => 'accept',
+  }
 }
