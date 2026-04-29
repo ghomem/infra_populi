0593ce208385fd219b43452a75e47c0b757b9845
PrinceSalatyell <salatyelll14@gmail.com>
2026-04-29 11:47:14 +0100

    P8: migrate puppet_infrastructure::firewall_addon_web

--- manifests/firewall_addon_web.pp
+++ manifests/firewall_addon_web.pp
@@ -1,17 +1,16 @@
-### Purpose ########
-# This class provides COMPLEMENTARY firewall configurations for web traffic
-### Warnings #######
-# Do not forget to do the main firewall configuration,
-# either by including one of our base classes
-# (puppet_infrastructure::firewall or puppet_infrastructure::firewall_secure)
-# or trough some other alternative (e.g. bash scripts).
-### Dependencies ###
-#  modules: puppetlabs-firewall
+# @summary Adds HTTP and HTTPS ingress rules for hosts with a managed firewall.
+#
+# @note This class only adds web traffic rules. Include a base firewall class,
+#   such as puppet_infrastructure::firewall or
+#   puppet_infrastructure::firewall_secure, or provide equivalent firewall
+#   setup before applying this addon.
+#
+# @see puppetlabs-firewall
 
 class puppet_infrastructure::firewall_addon_web {
 
   # webserver specific firewall rules - simple case
-  firewall { '200 accept http':  proto  => 'tcp', dport  => 80,  action => 'accept', }
-  firewall { '201 accept https': proto  => 'tcp', dport  => 443, action => 'accept', }
+  firewall { '200 accept http':  proto => 'tcp', dport => 80,  jump => 'accept', }
+  firewall { '201 accept https': proto => 'tcp', dport => 443, jump => 'accept', }
 
 }
