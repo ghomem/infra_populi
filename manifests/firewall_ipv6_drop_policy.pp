d6782484409cb7b4160a9c3cb8427608d6c9210f
PrinceSalatyell <salatyelll14@gmail.com>
2026-04-29 12:02:54 +0100

    P8: migrate puppet_infrastructure::firewall_ipv6_drop_policy

--- manifests/firewall_ipv6_drop_policy.pp
+++ manifests/firewall_ipv6_drop_policy.pp
@@ -1,11 +1,22 @@
-### Purpose ########
-# This class provides an add on to firewall_secure that changes IPv6 firewall policies to DROP
-
-### Dependencies ###
-#  modules: puppetlabs-firewall
+# @summary Enforce DROP as the default policy for selected IPv6 firewall chains.
+#
+# This class complements the firewall baseline by setting the IPv6 INPUT and
+# FORWARD chains to DROP while leaving rule management to the shared firewall
+# module.
+#
+# @see https://forge.puppet.com/modules/puppetlabs/firewall
+# @dependencies puppetlabs-firewall
 class puppet_infrastructure::firewall_ipv6_drop_policy {
-  
-  firewallchain { 'FORWARD:filter:IPv6': ensure => present, purge => true, policy => drop,  }
-  firewallchain { 'INPUT:filter:IPv6':   ensure => present, purge => true, policy => drop,  }
-  
+
+  firewallchain { 'FORWARD:filter:IPv6':
+    ensure => present,
+    purge  => true,
+    policy => drop,
+  }
+
+  firewallchain { 'INPUT:filter:IPv6':
+    ensure => present,
+    purge  => true,
+    policy => drop,
+  }
 }
