a6b1549dde0ad9a9ee7c77acd66753face28b58d
Salatiel Antonio <salatyelll14@gmail.com>
2026-07-01 13:49:32 +0100

    P8: migrate puppet_infrastructure::filesystem_yum

--- manifests/filesystem_yum.pp
+++ manifests/filesystem_yum.pp
@@ -1,26 +1,43 @@
-### Purpose ########
-# This class provides helper scripts for yum based system updates
+# @summary Installs yum helper scripts for system updates.
+#
+# @dependencies
+# - None. The class manages the local script directory inline.
 
 class puppet_infrastructure::filesystem_yum {
 
   $localdir = lookup('filesystem::localdir')
 
+  file { $localdir:
+    ensure => directory,
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { "${localdir}/bin":
+    ensure => directory,
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
+    require => File[$localdir],
+  }
+
   # security surface updates
   file { "${localdir}/bin/yum-update-surface.sh":
-  mode    => '0755',
-  owner   => 'root',
-  group   => 'root',
-  source  => 'puppet:///modules/puppet_infrastructure/yum/yum-update-surface.sh',
-  require => File[ "${localdir}/bin" ],
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/puppet_infrastructure/yum/yum-update-surface.sh',
+    require => File[ "${localdir}/bin" ],
   }
 
   # full updates
   file { "${localdir}/bin/yum-update-dist.sh":
-  mode    => '0755',
-  owner   => 'root',
-  group   => 'root',
-  source  => 'puppet:///modules/puppet_infrastructure/yum/yum-update-dist.sh',
-  require => File[ "${localdir}/bin" ],
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/puppet_infrastructure/yum/yum-update-dist.sh',
+    require => File[ "${localdir}/bin" ],
   }
 
 }
