b52da1fe1c0c54c6243d2f0a40bc2cfa2a4a0ca0
Salatiel Antonio <salatyelll14@gmail.com>
2026-07-26 21:23:47 +0100

    P8: migrate puppet_infrastructure::openvpn_set_vpn_passwd_file

--- manifests/openvpn_set_vpn_passwd_file.pp
+++ manifests/openvpn_set_vpn_passwd_file.pp
@@ -1,35 +1,30 @@
-### Purpose ########
-# Placement and configuration of a script that allows
-# settup of multiple openvpn connections on LD7 machines
-
-## Warnings ##
-# This class has only been tested on LD7 machines
-
-## Example ##
-# the file_path should be the complete path, just like a File declaration
+# @summary Deploys a password-setting script for named OpenVPN connections.
 #
+# The generated script prompts for a password and applies it to each configured
+# NetworkManager VPN connection. This defined type can manage multiple scripts.
 #
-# Example of placement in our local bin dir
-# puppet_infrastructure::openvpn_set_vpn_passwd_file { '/usr/local/AS/bin/set_vpn_passwd.sh':
-#   vpn_list => ['VPN1', 'VPN2', 'VPN3']
-# }
+# @example Place the script in a local binary directory
+#   puppet_infrastructure::openvpn_set_vpn_passwd_file { '/usr/local/AS/bin/set_vpn_passwd.sh':
+#     vpn_list => ['VPN1', 'VPN2', 'VPN3'],
+#   }
 #
+# @param vpn_list NetworkManager connection names; names must not contain spaces.
+# @param file_owner Owner and group assigned to the generated script.
+# @param file_path Absolute path of the generated script.
+#
+# @dependencies
+# - `puppet_infrastructure/openvpn/set_vpn_passwd.sh.erb` from this module
+# - `nmcli` and `kdialog` on the target system when the script is executed
+define puppet_infrastructure::openvpn_set_vpn_passwd_file (
+  Array $vpn_list = [],
+  $file_owner = 'root',
+  $file_path = $title,
+) {
 
-# this class is a define type because we might want
-# to manage more than one group of vpn connections
-define puppet_infrastructure::openvpn_set_vpn_passwd_file(
-
-    # vpn_list is an array containing the name of the configured connections
-    # vpn names should not have spaces
-    Array $vpn_list = [],
-    $file_owner = 'root',
-    $file_path = $title
-){
-
-    file { $file_path:
-        mode    => '0755',
-        owner   => $file_owner,
-        group   => $file_owner,
-        content => template('puppet_infrastructure/openvpn/set_vpn_passwd.sh.erb'),
-    }
+  file { $file_path:
+    mode    => '0755',
+    owner   => $file_owner,
+    group   => $file_owner,
+    content => template('puppet_infrastructure/openvpn/set_vpn_passwd.sh.erb'),
+  }
 }
