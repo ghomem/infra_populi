bd4e4749ab8b6c3e63e8379b241c4d2df7fcfcb0
Salatiel Antonio <salatyelll14@gmail.com>
2026-07-27 20:32:27 +0100

    P8: migrate puppet_infrastructure::filesystem_sec

--- manifests/filesystem_sec.pp
+++ manifests/filesystem_sec.pp
@@ -1,9 +1,23 @@
-### Purpose ########
-# This classe provides extra filesystem security by restricting a certain set of commands to a certain group
-define puppet_infrastructure::filesystem_sec ( $restricted_cmds = '' , $restricted_group = '' , $restricted_mode = '0750' )
-{
+# @summary Restricts selected commands to members of a designated group.
+#
+# Applies root ownership and the requested group and mode to each command path.
+# When no group is provided, RedHat-family systems use `wheel` and all other
+# systems use `sudo`.
+#
+# @param restricted_cmds Command path or paths to restrict.
+# @param restricted_group Group permitted to execute the commands.
+# @param restricted_mode File mode applied to the commands.
+#
+# @dependencies
+# - The structured `os.family` fact
+# - The target system's `wheel` or `sudo` group when no group is specified
+define puppet_infrastructure::filesystem_sec (
+  $restricted_cmds = '',
+  $restricted_group = '',
+  $restricted_mode = '0750',
+) {
   $os_family = $facts['os']['family']
-  
+
   if $restricted_group == '' {
     if $os_family == 'RedHat' {
       $restricted_group = 'wheel'
@@ -13,10 +27,8 @@ define puppet_infrastructure::filesystem_sec ( $restricted_cmds = '' , $restrict
   }
 
   file { $restricted_cmds:
-    owner => root,
+    owner => 'root',
     group => $restricted_group,
     mode  => $restricted_mode,
   }
-
 }
-
